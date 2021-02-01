47d8f0455ba91d11680f4155e3790cbb17eabfe3
“AdejokeOgunyinka” <ogunyinkajoke@yahoo.com>
2021-02-01 11:20:47 +0100

    Amended filter style and card style

--- src/components/searchBar/style.js
+++ src/components/searchBar/style.js
@@ -9,7 +9,8 @@ export const SearchStyle = styled.div`
     box-sizing: border-box;
     justify-content: center;
     align-items: center;
-    border: 1px solid grey;
+    border: none;
+    box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.2);
     border-radius: 25px;
     background-color: white;
     box-sizing: border-box;
